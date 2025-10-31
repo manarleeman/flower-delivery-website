70a52d7ffdf637622ff98fa732a26f82a47ed7c1
Manar Liman <manarleeman@gmail.com>
2025-10-31 21:19:56 +0100

    initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+	theme: {
+		extend: {
+			colors: {
+				secondary: "#121212",
+				tertiary: "#808080",
+			},
+		},
+	},
+	plugins: [],
+};
