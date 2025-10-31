b7cbe4e2923ef9d7454fa6046d208b8efee3c33e
Manar Liman <manarleeman@gmail.com>
2025-10-31 15:42:20 +0100

    feat: create footer component with contact information

---
+++ admin/src/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="text-center text-sm text-gray-700 mt-10 border-t pt-4 w-full max-w-3xl mx-auto">
+      <p className="font-medium">Flower Delivery App</p>
+      <p>
+        Created for ElevateHER Innovation Space Ltd
+        <br />
+        By [Aisha Abdullahi Liman]
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
