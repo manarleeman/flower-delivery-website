384c48c0b155e5839f90310276015ed09ed38e34
Manar Liman <manarleeman@gmail.com>
2025-10-31 22:05:39 +0100

    feat: setup base project structure and entry files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layout/mainlayout'
+import Home from './Pages/home'
+import AboutUs from './Pages/aboutUs'
 
+import SignIn from './components/signIn'
+import CategoryPage from './Pages/Category/CategoryPage'
+import ProductPage from './Pages/productpage'
+import Checkout from './Pages/checkout'
+import CheckoutPage from './Pages/CheckOut/checkOutpage'
+import OrderSummary from './Pages/CheckOut/orderSummary'
+import Payment from './Pages/CheckOut/payment'
+import ShippingDetails from './Pages/CheckOut/shipping'
+import SignUp from './Pages/signUp'
+import ForgotPassword from './Pages/Resetpassword'
+import OrderSuccess from './Pages/OrderSuccess'
+f4855a4565984904169c8c4ca6162ef9b581214f
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MainLayout />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: '/category/:categoryName',
+				element: <CategoryPage />,
+			},
+			{
+				path: '/product/:productId',
+				element: <ProductPage />,
+			},
+			{
+				path: '/aboutus',
+				element: <AboutUs />,
+			},
+			{
+				path: '/checkout',
+				element: <Checkout />,
+			},
+
+
+			{
+				path: '/shipping',
+				element: <ShippingDetails />,
+			},
+			{
+				path: '/orderSummary',
+				element: <OrderSummary />,
+			},
+			{
+				path: '/payment',
+				element: <Payment />,
+			},
+			{
+				path: '/order-success',
+				element: <OrderSuccess />,
+			},
+			{
+				path: '/forgotpassword',
+				element: <ForgotPassword />,
+			},
+		],
+	},
+])
+f4855a4565984904169c8c4ca6162ef9b581214f
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<RouterProvider router={router} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		// <div className="bg-red-800 h-screen">
+		// 	<h1>Flower Delivery Service</h1>
+		// </div>
+	)
 }
 
 export default App
